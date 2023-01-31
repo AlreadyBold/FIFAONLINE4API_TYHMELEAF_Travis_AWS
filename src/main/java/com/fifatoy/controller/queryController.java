e3841fa47a7fc880abbb2e075c5b67e6bbf52bf5
jh.lee2 <partner.jh.lee2@kyobobook.com>
2023-01-31 17:59:06 +0900

    등급정보 출력, 기타

--- src/main/java/com/fifatoy/controller/queryController.java
+++ src/main/java/com/fifatoy/controller/queryController.java
@@ -2,14 +2,18 @@ package com.fifatoy.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import com.fifatoy.util.APICALLUTIL;
+
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -27,6 +31,7 @@ public class queryController {
         String url = "https://static.api.nexon.co.kr/fifaonline4/latest/division.json";
         ArrayList<Map<String, Object>> divisionMap = new ArrayList<Map<String, Object>>();
         divisionMap = (ArrayList<Map<String, Object>>) apicallutil.NotKey(url);
+
         // 매치타입 가져오기 API 호출 하여 맵에 저장
         url = "https://static.api.nexon.co.kr/fifaonline4/latest/matchtype.json";
         ArrayList<Map<String, Object>> matchTypeMap = new ArrayList<Map<String, Object>>();
@@ -46,31 +51,30 @@ public class queryController {
         url = "https://api.nexon.co.kr/fifaonline4/v1.0/users/" + accessId + "/maxdivision";
         ArrayList<Map<String, Object>> resultListMap = new ArrayList<Map<String, Object>>();
         resultListMap = (ArrayList<Map<String, Object>>) apicallutil.UseKeyArray(url, apiKey);
-        String divisionType = String.valueOf(resultListMap.get(0).get("matchType"));
-        String divisionGrade = String.valueOf(resultListMap.get(0).get("division"));
-
+        // 모델에 삽입
+        ArrayList<Map<String, Object>> matchInfo = new ArrayList<Map<String, Object>>();
         for (int i = 0; i < resultListMap.size(); i++) {
+            Map<String, Object> userInfo = new HashMap<String, Object>();
             for (int j = 0; j < matchTypeMap.size(); j++) {
                 if (resultListMap.get(i).get("matchType").equals(matchTypeMap.get(j).get("matchtype"))) {
-                    session.setAttribute("matchtype" + i + "k", String.valueOf(matchTypeMap.get(j).get("desc")));
+                    userInfo.put("matchType", String.valueOf(matchTypeMap.get(j).get("desc")));
                     break;
                 }
             }
             for (int j = 0; j < divisionMap.size(); j++) {
                 if (resultListMap.get(i).get("division").equals(divisionMap.get(j).get("divisionId"))) {
-                    session.setAttribute("division" + i + "k", String.valueOf(divisionMap.get(j).get("divisionName")));
+                    userInfo.put("grade", String.valueOf(divisionMap.get(j).get("divisionName")));
                     break;
                 }
             }
+            matchInfo.add(userInfo);
         }
-
+        model.addAttribute("matchInfo", matchInfo);
         return "query";
-
     }
 
     @GetMapping("/queryhome")
     public String queryhome(Model model) {
         return "query";
     }
-    // https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname={nickname}
 }
