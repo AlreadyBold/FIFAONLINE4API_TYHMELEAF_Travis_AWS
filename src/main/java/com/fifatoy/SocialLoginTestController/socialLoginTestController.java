f31a774afdc87b94156615f73f1ecadabae55ff7
AlreadyBoldMACbook <junheong@nate.con>
2023-07-01 04:00:10 +0900

    소셜로그인 테스트 준비

---
+++ src/main/java/com/fifatoy/SocialLoginTestController/socialLoginTestController.java
@@ -0,0 +1,30 @@
+package com.fifatoy.SocialLoginTestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/social")
+public class socialLoginTestController {
+
+    /**
+     * 카카오 callback
+     * [GET] /social/kakaotest
+     */
+    @ResponseBody
+    @GetMapping("/kakaotest")
+    public void kakaoCallback(@RequestParam String code) {
+
+        // 인가 코드 
+        System.out.println(code);
+    }
+}
+
+
--- src/main/java/com/fifatoy/controller/welcomecontroller.java
+++ src/main/java/com/fifatoy/controller/welcomecontroller.java
@@ -8,6 +8,7 @@ public class welcomecontroller {
 
     @GetMapping("/index")
     public String index() {
+        
         return "/";
     }
 
