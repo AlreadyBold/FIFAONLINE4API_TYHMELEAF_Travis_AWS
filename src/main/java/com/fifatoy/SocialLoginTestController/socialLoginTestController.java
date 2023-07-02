4606f00ace4f46b687eb1d96484db10f6a6574a8
AlreadyBOLD <junheong@nate.com>
2023-07-02 23:43:46 +0900

    소셜로그인 컨트롤러 셋팅

--- src/main/java/com/fifatoy/SocialLoginTestController/socialLoginTestController.java
+++ src/main/java/com/fifatoy/SocialLoginTestController/socialLoginTestController.java
@@ -15,18 +15,40 @@ import lombok.AllArgsConstructor;
 public class socialLoginTestController {
 
     /**
-     * 카카오 callback
+     * kakao callback
      * [GET] /social/kakaotest
      */
     @ResponseBody
     @GetMapping("/kakaotest")
     public void kakaoCallback(@RequestParam String code) {
-        
-
-        
         // 인가 코드 
         System.out.println(code);
     }
+
+
+    /**
+     * google callback
+     * [GET] /social/googletest
+     */
+    @ResponseBody
+    @GetMapping("/googletest")
+    public void googleCallback(@RequestParam String code) {
+        // 인가 코드 
+        System.out.println(code);
+    }
+
+
+    /**
+     * naver callback
+     * [GET] /social/navertest
+     */
+    @ResponseBody
+    @GetMapping("/navertest")
+    public void naverCallback(@RequestParam String code) {
+        // 인가 코드 
+        System.out.println(code);
+    }
+
 }
 
 
