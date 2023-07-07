30873b19f1bb72098a4f48172fbe22b2263294e3
jh.lee2 <junheong@nate.com>
2023-07-07 16:59:59 +0900

    .

---
+++ src/main/java/com/fifatoy/controller/matchController.java
@@ -0,0 +1,37 @@
+package com.fifatoy.controller;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fifatoy.util.APICALLUTIL;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/Match")
+public class matchController {
+    // API키
+    @Value("${NexonApiKey}")
+    private String apiKey;
+
+    // UTIL
+    APICALLUTIL apicallutil = new APICALLUTIL();
+
+    // 매치타입 조회 URL
+    String MatchTypeUrl = "https://static.api.nexon.co.kr/fifaonline4/latest/matchtype.json";
+
+    @GetMapping("/MatchInfo")
+    public String MatchInfo(HttpSession session, Model model, @RequestParam int code) {
+        // Map<Integer, String> arr = ((Object) apicallutil.NotKeyArray(MatchTypeUrl));
+
+        return "currentTradeinfo";
+    }
+
+}
