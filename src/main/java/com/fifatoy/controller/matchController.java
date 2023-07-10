b6d6f6b294e8d913bd32cb3f56579d231cb85c71
jh.lee2 <junheong@nate.com>
2023-07-10 11:17:20 +0900

    JPA 제거

--- src/main/java/com/fifatoy/controller/matchController.java
+++ src/main/java/com/fifatoy/controller/matchController.java
@@ -1,10 +1,8 @@
 package com.fifatoy.controller;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
